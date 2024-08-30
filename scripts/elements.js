7123d10e0ab417ad3a870b56d66e18ae733ea0dc
Iris <87090296+Iris-TheRainbow@users.noreply.github.com>
2024-08-30 10:35:41 -0500

    testing

---
+++ scripts/elements.js
@@ -0,0 +1,35 @@
+function generateElements(){
+    document.getElementById("footer").innerHTML = `
+            <div class="footerContainer">
+                <hr>
+                <div>
+                    <button onclick="window.location.href='robots'">About Me</button>
+                </div>
+                <div>
+                    <button onclick="window.location.href='sponsors'">Asignments</button>
+                </div>
+                <div>
+                    <button onclick="window.location.href='gallery'">Gallery</button>
+                </div>
+                <div>
+                    <button onclick="window.location.href='contactme'">Contact Me</button>
+                <div style="float: right;">
+                    <button onclick="window.location.href='mailto:snazzsinclair@gmail.com'">Contact me!</button>
+                </div>
+            </div>
+    `
+    document.getElementById("headder").innerHTML = `
+        <div class="headderContainer">
+            <div class="headderColumn" style="width: fit-content; display: flex;">
+                <button onclick="window.location.href='/'" style="font-size: 350%; width: fit-content; max-width: 45%; background-color: #202020;"> Jazz's Portfolio</button>
+            </div>
+        </div>`
+
+    if (navigator.userAgent.match(/Android/i)
+        || navigator.userAgent.match(/webOS/i)
+        || navigator.userAgent.match(/iPhone/i)
+        || navigator.userAgent.match(/iPad/i)
+        || navigator.userAgent.match(/iPod/i)){
+        var mobile = true;
+    }
+}
