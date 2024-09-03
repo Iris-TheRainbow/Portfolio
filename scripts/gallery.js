4529b5b760debca5a5ba63e6a6a64956dbe2d0e7
Iris <87090296+Iris-TheRainbow@users.noreply.github.com>
2024-09-03 09:12:12 -0500

    new gallery format

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,13 +1,29 @@
 function generateGallery(){
-    let string = "";
+    let string0 = "";
+    let string1 = "";
+    let string2 = "";
     fetch("/galleryimgs.txt")
         .then((res) => res.text())
         .then((text) => {
             let imgs = text.split("\n");
             for (let i = 0; i < (imgs.length - 1); i++){
-                string += "<img src=\"" + imgs[i] + "\"> "
+                if (x == 0) {
+                    string0 += "<img src=\"" + imgs[i] + "\"> "
+                    x += 1
+                }
+                if (x == 1){
+                    string1 += "<img src=\"" + imgs[i] + "\"> "
+                    x += 1
+                }
+                if (x == 2){
+                    string2 += "<img src=\"" + imgs[i] + "\"> "
+                    x = 0
+                }
             }
-            document.getElementById("images").innerHTML = string;
+            document.getElementById("images0").innerHTML = string0;
+            document.getElementById("images1").innerHTML = string1;
+            document.getElementById("images2").innerHTML = string2;
+
         })
         .catch((e) => console.error(e));
 
